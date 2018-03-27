bb36b098f06095d770272738525218833236c6c8
Garnett Puett and Matt Miller <matt@>
2018-03-27 13:24:07 -0700

    repurpose for factorial calculations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(function() {
-  var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "y"]
   $("#form").submit(function(event) {
-    event.preventDefault();
-    $("#form").hide();
-    var userInput = $("#puzzle").val();
-    var userArray = userInput.split("");
-    for (var i = 0; i < userArray.length; i++) {
-      for (var j = 0; j < vowels.length; j++) {
-       if (userArray[i] === vowels[j]) {
-         userArray[i] = "-";
-        }
-      }
-     }
-     var output = userArray.join("");
-     $("#output").text(output);
+
+  var input = parseInt($("#input").val());
+  for (var index = (input-1); index > 0; index += -1) {
+    input *= index
+  }
+  event.preventDefault();
+  $("#output").text(input)
   });
 });
