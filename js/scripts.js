c007526a1d19177e3b5f9b02d4b5bba6f72c2507
Garnett Puett and Matt Miller <matt@>
2018-03-27 09:49:38 -0700

    Hide form field upon submission.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(function() {
   var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U", "y"]
   $("#form").submit(function(event) {
     event.preventDefault();
+    $("#form").hide();
     var userInput = $("#puzzle").val();
     var userArray = userInput.split("");
     for (var i = 0; i < userArray.length; i++) {
